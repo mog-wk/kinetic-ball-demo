a14543da6bb27950c45a83e0bd2f87bffd8a37e0
mog-wk <ms-work@mail.com>
2024-04-16 10:40:28 -0300

    added sphere acceleration

--- src/bodies/sphere.rs
+++ src/bodies/sphere.rs
@@ -9,10 +9,13 @@ use sdl2::pixels::Color;
 //#[derive(PhysicBody)]
 #[derive(Debug)]
 pub struct Sphere {
-    // physic options
+    // format options
     position: (i32, i32), // center position
     radius: i32,
+
+    // physic options
     velocity: (i32, i32),
+    acceleration: (i32, i32),
 
     // render options
     fill_color: Color,
@@ -20,34 +23,35 @@ pub struct Sphere {
 }
 impl Sphere {
     pub fn update(&mut self) {
-        self.position = (
-            self.position.0 + self.velocity.0,
-            self.position.1 + self.velocity.1,
-        );
-
-        //self.velocity = (self.drag.0, self.drag.1);
+        self.add_position(self.velocity);
+        self.add_velocity(self.acceleration);
+        self.set_acceleration((0, 0));
     }
 
     pub fn render(&self, canvas: &mut Canvas<Window>) {
         canvas.set_draw_color(self.fill_color);
-        shapes::draw_circle(canvas,
-            self.position.0,
-            self.position.1,
-            self.radius
-        );
+        shapes::draw_circle(canvas, self.position.0, self.position.1, self.radius);
         canvas.set_draw_color(self.border_color);
-        shapes::draw_circunference(canvas,
-            self.position.0,
-            self.position.1,
-            self.radius
-        );
+        shapes::draw_circunference(canvas, self.position.0, self.position.1, self.radius);
     }
 
+    pub fn set_position(&mut self, val: (i32, i32)) {
+        self.position = val;
+    }
+    pub fn add_position(&mut self, val: (i32, i32)) {
+        self.position = (self.position.0 + val.0, self.position.1 + val.1);
+    }
     pub fn set_velocity(&mut self, val: (i32, i32)) {
         self.velocity = val;
     }
     pub fn add_velocity(&mut self, val: (i32, i32)) {
         self.velocity = (self.velocity.0 + val.0, self.velocity.1 + val.1);
+    }
+    pub fn set_acceleration(&mut self, val: (i32, i32)) {
+        self.acceleration = val;
+    }
+    pub fn add_acceleration(&mut self, val: (i32, i32)) {
+        self.acceleration = (self.acceleration.0 + val.0, self.acceleration.1 + val.1);
     }
 }
 
@@ -60,6 +64,7 @@ impl std::default::Default for Sphere {
             ),
             radius: 48,
             velocity: (0, 0),
+            acceleration: (0, 0),
             fill_color: Color::RGB(127, 127, 127),
             border_color: Color::RGB(127, 127, 127),
         }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-
-mod error;
-mod utils;
 mod bodies;
 mod config;
+mod error;
+mod utils;
 
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
@@ -13,39 +12,39 @@ fn main() -> Result<(), error::Error> {
 
     let video_ctx = sdl_ctx.video()?;
 
-    let app_window = video_ctx.window("Kinetic sim",
-        config::Window::WIDTH,
-        config::Window::HEIGHT
-        )
+    let app_window = video_ctx
+        .window("Kinetic sim", config::Window::WIDTH, config::Window::HEIGHT)
         .position(0, 0)
         .resizable()
         .opengl()
         .build()?;
 
-    let mut canvas = app_window.into_canvas()
-        .build()?;
+    let mut canvas = app_window.into_canvas().build()?;
 
     let canvas_box = bodies::rect::CanvasBox::default();
 
-    // dev test 
+    // dev test
 
     let mut sp = bodies::sphere::new();
 
-        sp.add_velocity((0, 2));
+    sp.set_acceleration((0, 2));
 
     let mut event_pump = sdl_ctx.event_pump()?;
 
     'run: loop {
         for event in event_pump.poll_iter() {
             match event {
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } | Event::Quit {..} => break 'run,
+                Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                }
+                | Event::Quit { .. } => break 'run,
                 _ => println!("{:?}", event),
             }
         }
         // Process
 
         sp.update();
-
 
         // Render
 
@@ -55,12 +54,9 @@ fn main() -> Result<(), error::Error> {
 
         canvas_box.render(&mut canvas);
 
-
         sp.render(&mut canvas);
 
         canvas.present();
-
-
 
         //std::thread::sleep(std::time::Duration::from_millis(200));
         std::thread::sleep(std::time::Duration::new(0, 1_000_000_000_u32 / 60));
